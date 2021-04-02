be4c38a2b87d60a212762c7d90c627235e623f91
Sparr0w <72567141+SPARR0W1@users.noreply.github.com>
2021-04-02 23:29:34 +0300

    Add files via upload

---
+++ PhoneBook.py
@@ -0,0 +1,25 @@
+#Import to module
+import pickle
+#Uploading contacts
+ab = pickle.load(open("ab_dumb.p", "rb"))
+def contactlist():
+	#Contact list
+	for name, email in ab.items():
+		print("Contact {} with address {}.".format(name, email))
+
+def addcontact():
+	#Add contact
+	name = input("Name:")
+	email = input("Email:")
+	ab[name] = email
+def savecontact():
+	#Saving contacts
+	pickle.dump(ab, open("ab_dumb.p", "wb"))
+def addedcontact():
+	#The contact that was added
+	print("Added to the contact list ", name)
+
+addcontact()
+contactlist()
+addedcontact()
+savecontact()
